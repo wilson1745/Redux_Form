28dab24d19698af48eb106a7c7eb2bf26f769f68
Lo Wei-Shen <38279941+wilson1745@users.noreply.github.com>
2019-02-17 11:34:57 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Values } from "redux-form-website-template";
+import store from "./store";
+import showResults from "./showResults";
+import SimpleForm from "../components/SimpleForm";
+import SyncValidationForm from "../components/SyncValidationForm";
+
+//import "./styles.css";
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div style={{ padding: 15 }}>
+      <h2>Simple Form</h2>
+      <SimpleForm onSubmit={showResults} />
+      <Values form="SimpleForm" />
+      <h2>Synchronous Validation</h2>
+      <SyncValidationForm onSubmit={showResults} />
+      <Values form="SyncValidationForm" />
+    </div>
+  </Provider>,
+  rootElement
+);
